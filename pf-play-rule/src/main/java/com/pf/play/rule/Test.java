c913bae132de2b2bf49fca48442cbddc7d5f550c
df-HelloWorld <duanfeng_1712@qq.com>
2019-11-20 20:37:54 +0800

    测试提交代码

---
+++ pf-play-rule/src/main/java/com/pf/play/rule/Test.java
@@ -0,0 +1,13 @@
+package com.pf.play.rule;
+
+/**
+ * @Description TODO
+ * @Author yoko
+ * @Date 2019/11/20 20:36
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String [] args){
+        System.out.println("HelloWorld !");
+    }
+}

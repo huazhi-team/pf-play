85d3960f555675923c264e0fa7b98bf910952dbd
tangzhulong12 <345061813@qq.com>
2019-11-20 20:42:20 +0800

    test

---
+++ pf-play-rule/src/main/java/com/pf/play/rule/util/TaskMeThod.java
@@ -0,0 +1,29 @@
+package com.pf.play.rule.util;
+
+import com.pf.play.rule.core.model.UvitalityValueList;
+
+/**
+ * @Description TODO
+ * @Author long
+ * @Date 2019/11/20 20:41
+ * @Version 1.0
+ */
+public class TaskMeThod {
+    /**
+     * @Description: TODO
+     * @param memberId
+    * @param type
+    * @param symbo
+    * @param activeValue
+     * @return com.pf.play.rule.core.model.UvitalityValueList
+     * @author long
+     * @date 2019/11/20 20:33
+     */
+    public static UvitalityValueList pottingVitalityValue(Integer memberId, Integer type,
+                                                          Integer symbo, Float activeValue){
+
+
+        UvitalityValueList  uvitalityValueList = new  UvitalityValueList();
+        return   uvitalityValueList;
+    }
+}

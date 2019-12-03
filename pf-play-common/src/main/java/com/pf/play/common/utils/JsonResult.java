647f5a82a672988e786b011be0a3bb2fcc90d275
tangzhulong12 <345061813@qq.com>
2019-12-03 10:29:31 +0800

    提交测试

--- pf-play-common/src/main/java/com/pf/play/common/utils/JsonResult.java
+++ pf-play-common/src/main/java/com/pf/play/common/utils/JsonResult.java
@@ -15,9 +15,9 @@ public class JsonResult<T> implements Serializable {
 	private static final long serialVersionUID = -110632011249276581L;
 	// 返回状态码:0代表正常，非零代表错误，-1表示数据为空
 	private String errcode;
-	private boolean message;
-	private String content;
-	private T data;
+	private String message;
+//	private String content;
+	private T content;
 	private String sgid;
 	private String cgid;
 
@@ -31,8 +31,8 @@ public class JsonResult<T> implements Serializable {
 	public static <T> JsonResult<T> successResult(T data) {
 		JsonResult<T> jsonResult = new JsonResult<T>();
 		jsonResult.setErrcode("0");
-		jsonResult.setMessage(true);
-		jsonResult.setData(data);
+		jsonResult.setMessage("success");
+		jsonResult.setContent(data);
 		return jsonResult;
 	}
 
@@ -45,8 +45,8 @@ public class JsonResult<T> implements Serializable {
 	public static <T> JsonResult<T> successResult(T data, String cgid, String sgid) {
 		JsonResult<T> jsonResult = new JsonResult<T>();
 		jsonResult.setErrcode("0");
-		jsonResult.setMessage(true);
-		jsonResult.setData(data);
+		jsonResult.setMessage("success");
+		jsonResult.setContent(data);
 		jsonResult.setCgid(cgid);
 		jsonResult.setSgid(sgid);
 		return jsonResult;
@@ -64,9 +64,9 @@ public class JsonResult<T> implements Serializable {
 	public static <T> JsonResult<T> failedResult(String msg, String code) {
 		JsonResult<T> jsonResult = new JsonResult<T>();
 		jsonResult.setErrcode(code);
-		jsonResult.setMessage(false);
-		jsonResult.setContent(msg);
-		jsonResult.setData(null);
+		jsonResult.setMessage(msg);
+//		jsonResult.setContent(msg);
+		jsonResult.setContent(null);
 		return jsonResult;
 	}
 
@@ -81,9 +81,9 @@ public class JsonResult<T> implements Serializable {
 	public static <T> JsonResult<T> failedResult(String msg, String code, String cgid, String sgid) {
 		JsonResult<T> jsonResult = new JsonResult<T>();
 		jsonResult.setErrcode(code);
-		jsonResult.setMessage(false);
-		jsonResult.setContent(msg);
-		jsonResult.setData(null);
+		jsonResult.setMessage(msg);
+		jsonResult.setContent(null);
+//		jsonResult.setData(null);
 		jsonResult.setCgid(cgid);
 		jsonResult.setSgid(sgid);
 		return jsonResult;
@@ -99,9 +99,9 @@ public class JsonResult<T> implements Serializable {
 	public static <T> JsonResult<T> failedResult(T data, String msg, String resultCode) {
 		JsonResult<T> jsonResult = new JsonResult<T>();
 		jsonResult.setErrcode(resultCode);
-		jsonResult.setMessage(false);
-		jsonResult.setContent(msg);
-		jsonResult.setData(data);
+		jsonResult.setMessage(msg);
+		jsonResult.setContent(null);
+//		jsonResult.setData(data);
 		return jsonResult;
 	}
 
@@ -114,28 +114,20 @@ public class JsonResult<T> implements Serializable {
 		this.errcode = errcode;
 	}
 
-	public boolean isMessage() {
+
+
+
+
+	public String getMessage() {
 		return message;
 	}
 
-	public void setMessage(boolean message) {
+	public void setMessage(String message) {
 		this.message = message;
 	}
 
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
+	public void setContent(T content) {
 		this.content = content;
-	}
-
-	public T getData() {
-		return data;
-	}
-
-	public void setData(T data) {
-		this.data = data;
 	}
 
 	public String getSgid() {

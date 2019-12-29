0696dbb9cf77a4af07ffffc9602427e4b8f36c07
df-HelloWorld <duanfeng_1712@qq.com>
2019-12-29 13:40:28 +0800

    1.阿里云短信模块：修改验证码首位数如果是0则替换成1

--- pf-play-rule/src/main/java/com/pf/play/rule/core/common/utils/SendSmsUtils.java
+++ pf-play-rule/src/main/java/com/pf/play/rule/core/common/utils/SendSmsUtils.java
@@ -27,6 +27,10 @@ public class SendSmsUtils {
 
     /**
      * @Description: 给手机号发送短信验证码
+     * <p>
+     *     阿里发送短信模块：首位数是0，则阿里短信会默认去掉0；
+     *     这里添加了一个字符串替换：如果首位数是0，则把首位数替换成1
+     * </p>
      * @param key - 要缓存的key
      * @param phoneNum - 要发送的手机号
      * @return String - 返回短信验证码；如果为空则说明发送验证码失败
@@ -37,6 +41,9 @@ public class SendSmsUtils {
         // 生成4位数随机数
         int num = (int)((Math.random()*9+1)*1000);
         String smsCode = String.valueOf(num);
+        if (smsCode.startsWith("0")){
+            smsCode = smsCode.replaceFirst("0" , "1");
+        }
         boolean flag = SendSms.aliSendSms(phoneNum, smsCode);
         if (flag){
             // redis存储验证码
@@ -71,6 +78,11 @@ public class SendSmsUtils {
     public static void main(String[] args) {
         int num = (int)((Math.random()*9+1)*1000);
         log.info("num:" + num);
+        String str = "0001";
+        if (str.startsWith("0")){
+            str = str.replaceFirst("0" , "1");
+        }
+        log.info("str:" + str);
     }
 
 }
